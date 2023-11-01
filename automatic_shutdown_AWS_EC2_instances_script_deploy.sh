c5eb684b4d2bcd9dfb98d2e2c280ce7ac39f90ad
Josuerinho <44467032+Josuerinho@users.noreply.github.com>
2023-11-01 15:58:55 -0400

    modifications to shutdown script

--- automatic_shutdown_AWS_EC2_instances_script_deploy.sh
+++ automatic_shutdown_AWS_EC2_instances_script_deploy.sh
@@ -4,16 +4,13 @@
 
 count=0
 
-user="ubuntu" # or whatever name you are using for your user
-
-process=$(top -bn 1 | sed 1,7d | grep $user | awk '{print $2}' | uniq)
-#echo $user
+process=$(top -bn 1 | sed 1,7d | grep $USER | awk '{print $2}' | uniq)
 
 if [[ $process ]]
 then
 	((count+=1))
 fi
 
-login=$(who | awk '{print $1}' | grep $user | uniq)
+login=$(who | awk '{print $1}' | grep $USER | uniq)
 
 if [[ $count == 0 ]] && [[ -z $login ]]; then sudo shutdown now;  fi
